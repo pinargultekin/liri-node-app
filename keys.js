10cb7c03f3f95e4f805b47c40bcfc0129ae1d78c
29bucuk <pinargultekin@yahoo.com>
2019-07-03 20:06:08 -0400

    Concert-this function added

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+console.log('\n\nThis is loaded!');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
--- liri.js
+++ liri.js
@@ -1,32 +1,65 @@
 require("dotenv").config();
 
-var fs = require("fs");
-var Spotify = require("node-spotify-api");
-var keys = require("./keys");
-var axios = require("axios");
-var moment = require("moment");
+var fs = require('fs');
+
+var Spotify = require('node-spotify-api');
+
+var keys = require("./keys.js");
+
+const axios = require('axios');
+
+var moment = require('moment');
 moment().format();
 
-var userCommand = process.argv[2];
-var userInput = function () {
+var cmd = process.argv[2];
+var input = function () {
     var name = [];
     for (var i = 3; i < process.argv.length; i++) {
         name.push(process.argv[i]);
     }
     return name.join(" ");
-}
+}();
 
-switch (userCommand) {
+switch (cmd) {
     case "concert-this":
-        concertThis(userInput);
+        concert(input);
         break;
     case "spotify-this-song":
-        spotifyThisSong(userInput);
+        spotifyThis(input);
         break;
     case "movie-this":
-        movieThis(userInput);
+        movie(input);
         break;
     case "do-what-it-says":
-        doWhatItSays(userInput);
+        doThis(input);
         break;
-}
+    default: console.log("\nPlease check your statement.");
+};
+
+//Functions
+
+
+//Concert-this
+function concert(input) {
+    var url = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+    axios.get(url)
+        .then(function (response) {
+
+            var Data = response.data;
+            var results;
+
+            for (var i = 0; i < Data.length; i++) {
+
+                var results = Data[i];
+
+                console.log("\nResults for " + input + "\nVenue name: " + results.venue.name + "\nVenue location: " + results.venue.city + ", " + results.venue.region +" " +results.venue.country+ "\nEvent date: " + moment(results.datetime).format("MM-DD-YYYY") + "\n\n---------------------------\n");
+                // results.push("\nResults for "+ input + "\nVenue name: " + results.venue.name + "\nVenue location: " + results.venue.city + ", " + results.venue.region + "\nEvent date: "+ moment(results.datetime).format("MM-DD-YYYY")+"\n\n---------------------------\n");
+            }
+
+            // fs.appendFile("log.txt", results.join(""), function(err, results){
+            //     if (err) throw err;
+            // })
+        });
+};
+
+
